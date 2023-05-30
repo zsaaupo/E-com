b99b60b6e11ba1af52de891558593e623af491c0
Zsa Aupo <zakariasarker407@gmail.com>
2023-05-30 14:27:16 +0600

    field checking method

--- login/models.py
+++ login/models.py
@@ -72,3 +72,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user + "'s profile"
+    
+    def is_fully_filled(self):
+        """for check all fields are filled"""
+        field_names = [f.name for f in self._meta.get_fields()]
+        
+        for field_name in field_names:
+            value = getattr(self, field_name)
+            if value is None or value=='':
+                return False
