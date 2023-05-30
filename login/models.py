26383f79c9c277ccdca6dbede8d4f7ff0d9c6f62
Zsa Aupo <zakariasarker407@gmail.com>
2023-05-30 12:51:49 +0600

    superuser creation method added

--- login/models.py
+++ login/models.py
@@ -19,3 +19,23 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        """create SuperUser with email, password and extra default fields"""
+        
+        
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser = True')
+
+        
+        return self._create_user(email, password, **extra_fields)
+        
+        
