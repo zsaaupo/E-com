353eb671fa8aa768d92394acfcfca96c360f3be2
Zsa Aupo <zakariasarker407@gmail.com>
2023-05-30 12:36:46 +0600

    created a custom user creation model

--- login/models.py
+++ login/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# for custom user model and admin pannel
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy
+
+
+class CustomUserManager(BaseUserManager):
+    """ Custom user manager : create a user by email and password"""
+    
+    def _create_user(self, email, password, **extra_fields):
+        """create user with email and password"""
+        
+        if not email:
+            raise ValueError("The email must be set")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
