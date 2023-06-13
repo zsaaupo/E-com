56d866f89f17c3c0f47f85509a1250905814a711
zsaaupo <zakariasarker407@gmail.com>
2023-06-13 08:09:13 +0600

    views created for authentication

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from login import views
 
 app_name = 'login'
 
 urlpatterns = [
-    
+    path('account/signup/', views.sign_up, name='signup')
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.http import HttpResponse
+
+# authentication
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
 
+# forms and models
+from login.models import Profile
+from login.forms import ProfileForm, SignUpForm
+
+
+def sign_up(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse())
+    
+    return render(request, 'login/sign_up.html', context={'form':form})
+
+def login_user(request):
+    form = AuthenticationForm()
+    if request == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse("logged in")
+    
+    return render(request, 'login/login.html', context={'form':form})
